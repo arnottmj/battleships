bd20daffbfda353298a74c898a915d752f86bc36
TheoLeanse <theoleanse@gmail.com>
2015-06-15 11:03:29 +0100

    adds test for Board#place

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,9 @@
 require 'board'
 
-describe Board do 
+describe Board do
   
+  it "responds to #place with two arguments" do
+    expect(subject).to respond_to(:place).with(2).arguments
+  end
+
 end
