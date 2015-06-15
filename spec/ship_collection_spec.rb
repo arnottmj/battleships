c72cc01916e54f7abed42d8e01f788181bb02758
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-15 14:33:56 +0100

    edited test to fix bugs

--- spec/ship_collection_spec.rb
+++ spec/ship_collection_spec.rb
@@ -1,11 +1,12 @@
-
+require 'ship_collection'
 
 describe ShipCollection do
 
 
   it 'creates a group of 5 boats of different sizes' do
-    fleet = ShipCollection.new
-    expect(fleet.count).to eq(5)
+    collection = ShipCollection.new
+    fleet = collection.create
+    expect(fleet.length).to eq(5)
   end
 
 
