c1639875ac5c086f2797095a14d98f547f953c95
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-15 11:13:00 +0100

    updated tests to test new method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,9 @@
 describe Board do
   it 'creates a board with 10 columns' do
-    expect(subject.count).to eq 10
+    expect(subject.create.count).to eq 10
   end
 
   it 'creates a board with 10 rows' do
-  expect(subject[0].count).to eq 10
+  expect(subject.create[0].count).to eq 10
   end
 end
