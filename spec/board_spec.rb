68772676e7ac723a67a464157398d05d09d15240
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-15 11:02:13 +0100

    fixing syntax error

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,6 +3,7 @@ describe Board do
     expect(subject.count).to eq 10
   end
 
-  it 'creates a board with 10 rows'
+  it 'creates a board with 10 rows' do
   expect(subject[0].count).to eq 10
+  end
 end
