6f5d8c9d50e5bfb8d5fe2a175a4c3bc43874c4cc
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-15 18:11:33 +0100

    added new test to check if the ship can receive a hit

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,10 +1,14 @@
 require 'ship'
 
 describe Ship do
-
+  let(:ship) {Ship.new(1,1)}
   it 'saves a position' do
-    ship = Ship.new(1,1)
     expect(ship.position).to eq ([1,1])
+  end
+
+  it 'has a hit value' do
+    ship.receive_hit
+    expect(ship).to be_hit
   end
 
 end
