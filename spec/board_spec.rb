c09eb8cf700278893b0d42d22631cce6c18bbda8
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-15 11:52:12 +0100

    added test to test adding to the grid

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,22 @@
+require 'board'
+
 describe Board do
   it 'creates a board with 10 columns' do
     expect(subject.create.count).to eq 10
   end
 
   it 'creates a board with 10 rows' do
-  expect(subject.create[0].count).to eq 10
+    expect(subject.create[0].count).to eq 10
   end
+
+  describe 'Add' do
+    it 'adds a boat with size 1' do
+      board = Board.new
+      board.create
+      expect(board.add(0,0)).to eq 1
+    end
+  end
+
+
+
 end
