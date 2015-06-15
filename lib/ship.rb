533962e94ebe6c18c34bd7a0612f18ad83cefaff
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-15 14:17:52 +0100

    Adds tests for response to #size= method and for size= to set the ship size

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,5 +1,7 @@
 class Ship
 
-  attr_reader :size
+#attr_accessor :size
+
+
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,7 +2,26 @@ require 'ship'
 
 describe Ship do
 
-  it { is_expected.to respond_to :size }
+  it { is_expected.to respond_to :size= }
+
+  # it "responds to #new with one arguments" do
+  #   expect(subject).to respond_to(:new).with(1).argument
+  # end  
+
+  describe "size=" do
+    
+    it "sets the size of the ship" do
+      subject.size=5
+      expect(subject.size).to eq(5)
+    end
+  end
+
+  # describe "#size" do
+  #   it "responds with the appropriate size" do
+  #     subject.size=5
+  #     expect(subject.size).to eq(5)
+  #   end
+  # end
 
 
 end
