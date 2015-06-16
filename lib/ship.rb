73c00cb1227a24054a2c343073831abc4d7b976c
TheoLeanse <theoleanse@gmail.com>
2015-06-16 16:58:12 +0100

    adds #hit, #sunk?, #hit?

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,5 +1,22 @@
 class Ship
 
-  attr_accessor :size
+  attr_reader :size
+  def initialize(size = 1)
+    @size = size
+    @hit = false
+    @life = size
+  end
 
+  def hit
+    @hit = true
+    @life -= 1
+  end
+
+  def hit?
+    @hit
+  end
+
+  def sunk?
+    @life == 0
+  end
 end
