91835534674d008b37442111de6281b80a772d1c
TheoLeanse <theoleanse@gmail.com>
2015-06-16 16:57:09 +0100

    adds test for ship and sunk methods

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,8 +2,14 @@ require 'ship'
 
 describe Ship do
 
-  it { is_expected.to respond_to :size= }
   it { is_expected.to respond_to :size }
+  it { is_expected.to respond_to :sunk?}
+
+  it 'responds to #hit' do
+
+    expect(subject).to respond_to :hit
+
+  end
 
   # it "responds to #new with one arguments" do
   #   expect(subject).to respond_to(:new).with(1).argument
@@ -12,9 +18,29 @@ describe Ship do
   describe "size=" do
 
     it "sets the size of the ship" do
-      subject.size=5
-      expect(subject.size).to eq(5)
+      ship = Ship.new(5)
+      expect(ship.size).to eq(5)
     end
+  end
+
+  describe 'hit' do
+
+    it 'registers a strike' do
+      subject.hit
+      expect(subject).to be_hit
+    end
+
+  end
+
+  describe 'sunk' do
+
+    it 'can get sunk' do
+      ship = Ship.new(3)
+      3.times { ship.hit }
+      expect(ship).to be_sunk
+
+    end
+
   end
 
   # describe "#size" do
