cca6a8763dbe38c4ffd58e12674ed33ae2d6f5b6
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-17 11:33:08 +0100

    collection now has a hash with the names of length five

--- lib/ship_builder.rb
+++ lib/ship_builder.rb
@@ -4,7 +4,11 @@ class ShipBuilder
 
   def initialize
 
-    @collection
+    @collection = {aircraft_carrier: 5,
+                   battleship:       4,
+                   submarine:        3,
+                   destroyer:        3,
+                   patrol_boat:      2}
   end
 
 end
