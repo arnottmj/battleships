566c85bd81b916cfbe13fe1536c7f3d263dbcf0e
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-17 11:49:19 +0100

    added new test to check for functionality of create

--- spec/ship_builder_spec.rb
+++ spec/ship_builder_spec.rb
@@ -9,13 +9,19 @@ describe ShipBuilder do
     expect(subject.collection.length).to eq 5
   end
 
-  it 'responds to create' do
-    expect(subject).to respond_to :create
+  describe 'create' do
+    it 'responds to create' do
+      expect(subject).to respond_to :create
+    end
+
+    it 'creates a ship' do
+      expect(subject.create(:aircraft_carrier)).to be_instance_of(Ship)
+    end
   end
 
   describe 'get_size' do
     it 'return size of a given symbol' do
-      expect(subject.get_size(:aricraft_carrier)).to eq 5
+      expect(subject.get_size(:aircraft_carrier)).to eq 5
     end
   end
 
